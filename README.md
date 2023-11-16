4ea87169dfa83d52ddbf8aef0502085890ced606
TheWicklowWolf <111055425+TheWicklowWolf@users.noreply.github.com>
2023-11-16 14:09:41 +0000

    Create pyDuckDNS

---
+++ README.md
@@ -0,0 +1,27 @@
+![Build Status](https://github.com/TheWicklowWolf/pyDuckDNS/actions/workflows/main.yml/badge.svg)
+![Docker Pulls](https://img.shields.io/docker/pulls/thewicklowwolf/pyduckdns.svg)
+
+DuckDNS IP Updater.
+
+## Run using docker-compose
+
+```yaml
+version: "2.1"
+services:
+  pyduckdns:
+    image: thewicklowwolf/pyduckdns:latest
+    container_name: pyduckdns
+    environment:
+      - domains=abc123,xyz789
+      - duckdns_token=1234567890
+      - refresh_interval=600
+    volumes:
+      - /etc/localtime:/etc/localtime:ro
+    restart: unless-stopped
+```
+
+---
+
+
+https://hub.docker.com/r/thewicklowwolf/pyduckdns
+
---
+++ pyDuckDNS.py
@@ -0,0 +1,53 @@
+import os
+import time
+import logging
+import ipaddress
+import requests
+
+logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
+logger = logging.getLogger()
+
+api_endpoints = ["https://api.ipify.org", "http://wtfismyip.com/text"]
+domains = os.environ.get("domains", "domainB,domainA")
+token = os.environ.get("duckdns_token", "123")
+refresh_interval = float(os.environ.get("refresh_interval", 600))
+
+
+def get_public_ip(api_endpoint):
+    try:
+        ip = requests.get(api_endpoint).text.replace("\n", "")
+        ipaddress.ip_address(ip)
+        logger.info(f"Found IP Address: {ip} from {api_endpoint}")
+        return ip.strip()
+    except Exception as e:
+        logger.error(f"Error getting public IP from {api_endpoint}, Error: {e}")
+        return None
+
+
+def update_duckdns(ip):
+    try:
+        url = f"https://www.duckdns.org/update?domains={domains}&token={token}&{'&ip=' + ip if ip else ''}&verbose=true"
+        req = requests.get(url)
+        if req.status_code == 200 and "OK" in req.content.decode():
+            response_state, response_ip_v4, response_ip_v6, response_text = req.content.decode().split("\n")
+            logger.info(f"Response: {response_state} -> {response_text} -> Domains: {domains} -> IP address: {response_ip_v4}")
+
+        else:
+            logger.error(f"Failed to update {domains} with IP address: {ip}")
+            logger.error(req.status_code)
+            logger.error(req.content)
+            logger.error(req.headers)
+
+    except Exception as e:
+        logger.error(f"Error updating DuckDNS: {e}")
+
+
+while True:
+    logger.info("Getting IP Address")
+    ip = next((ip for api in api_endpoints if (ip := get_public_ip(api)) is not None), None)
+    if not ip:
+        logger.warning("No valid IP Address Found, letting DuckDNS obtain it automatically")
+
+    update_duckdns(ip)
+    logger.info(f"Sleeping for {refresh_interval} seconds")
+    time.sleep(refresh_interval)
